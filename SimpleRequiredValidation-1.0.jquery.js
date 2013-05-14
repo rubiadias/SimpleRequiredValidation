9db955ded0eb5fc01ee7df9f67e0c44a09012900
Ricardo Dantas <ricardodantas@gmail.com>
2013-05-14 14:33:27 -0300

    / ‘SimpleRequiredValidation-1.0.jquery.js’

--- SimpleRequiredValidation-1.0.jquery.js
+++ SimpleRequiredValidation-1.0.jquery.js
@@ -1,26 +1,42 @@
-(function( $ ) {
-  $.fn.SimpleRequiredValidation = function() {
+(function($){
 
-	$('input,textarea,select').bind('blur',function(){
+	//Attach this new method to jQuery
+ 	$.fn.extend({
 
-		var $value = $.trim($(this).val());
+ 		//This is where you write your plugin's name
+ 		pluginname: function() {
 
-		if($(this).attr('required') && $value==''){
+			//Iterate over the current set of matched elements
+    		return this.each(function() {
 
-			$(this).css('border','1px solid red').focus();
 
-			if($('small#warningMessage').length <= 0)
-				$('<small id="warningMessage">Preencha o campo abaixo</small>').insertAfter(this);
 
-		}else{
+				$('input,textarea,select').bind('blur',function(){
 
-			$(this).css('border','1px solid #ccc');
+					var $value = $.trim($(this).val());
 
-			$('small#warningMessage').remove();
-		}
+					if($(this).attr('required') && $value==''){
 
+						$(this).css('border','1px solid red').focus();
+
+						if($('small#warningMessage').length <= 0)
+							$('<small id="warningMessage">Preencha o campo abaixo</small>').insertAfter(this);
+
+					}else{
+
+						$(this).css('border','1px solid #ccc');
+
+						$('small#warningMessage').remove();
+					}
+				});
+
+
+
+    		});
+    	}
 	});
 
-  };
-
-})( jQuery );
+//pass jQuery to the function,
+//So that we will able to use any valid Javascript variable name
+//to replace "$" SIGN. But, we'll stick to $ (I like dollar sign: ) )
+})(jQuery);
